8c6b148e29ed66753707613f13db274c6b42c9b6
AmzalFoumi <147682418+AmzalFoumi@users.noreply.github.com>
2026-02-23 17:45:56 +0530

    module done

---
+++ module.js
@@ -0,0 +1,8 @@
+// const myModule = require("./my-module.js");
+
+// console.log(myModule.myFunction());
+
+//To tes this, I will use the OS module
+
+const os = require("os");
+console.log(os.platform());
